1661f9f5f6c48a0a5b976f4de028fe7d746c103f
tsablez <me@tsablezub.ru>
2026-03-31 16:27:56 +0300

    Create mathCtrlModule.js

---
+++ Modules/mathCtrlModule.js
@@ -0,0 +1,51 @@
+function planimetriyaThemes () {
+    planimetriyaList.classList.toggle('themesShowHide');
+    planimetriya.classList.toggle('themeSelected');
+}
+function vectoryThemes () {
+    vectoryList.classList.toggle('themesShowHide');
+    vectory.classList.toggle('themeSelected');
+}
+function stereometriyaThemes () {
+    stereometriyaList.classList.toggle('themesShowHide');
+    stereometriya.classList.toggle('themeSelected');
+}
+function teorverThemes () {
+    teorverList.classList.toggle('themesShowHide');
+    teorver.classList.toggle('themeSelected');
+}
+function verslozhsobThemes () {
+    verslozhsobList.classList.toggle('themesShowHide');
+    verslozhsob.classList.toggle('themeSelected');
+}
+function uravneniyaThemes () {
+    uravneniyaList.classList.toggle('themesShowHide');
+    uravneniya.classList.toggle('themeSelected');
+}
+function vychipreobThemes () {
+    vychipreobList.classList.toggle('themesShowHide');
+    vychipreob.classList.toggle('themeSelected');
+}
+function proizvipervoobThemes () {
+    proizvipervoobList.classList.toggle('themesShowHide');
+    proizvipervoob.classList.toggle('themeSelected');
+}
+function prikladzadThemes () {
+    prikladzadList.classList.toggle('themesShowHide');
+    prikladzad.classList.toggle('themeSelected');
+}
+function textzadThemes () {
+    textzadList.classList.toggle('themesShowHide');
+    textzad.classList.toggle('themeSelected');
+}
+function graphfuncThemes () {
+    graphfuncList.classList.toggle('themesShowHide');
+    graphfunc.classList.toggle('themeSelected');
+}
+function minmaxThemes () {
+    minmaxList.classList.toggle('themesShowHide');
+    minmax.classList.toggle('themeSelected');
+}
+function mnogogrannikThemes () {
+    mnogogrannikList.classList.toggle('themesShowHide');
+}
