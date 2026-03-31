1c5177c97cd34a4b85aa0a9e80786038edbc45a1
tsablez <me@tsablezub.ru>
2026-03-31 16:27:25 +0300

    Create btnCtrlModule.js

---
+++ Modules/btnCtrlModule.js
@@ -0,0 +1,12 @@
+function math () {
+    console.log('mathTrigger: normal');
+    mathList.classList.toggle('listShowHide');
+}
+function info () {
+    console.log('infoTrigger: normal');
+    infoList.classList.toggle('listShowHide');
+}
+function rus () {
+    console.log('rusTrigger: normal');
+    rusList.classList.toggle('listShowHide');
+}
