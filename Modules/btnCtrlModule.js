3559dd7f62d46c400bb9ddd47675a70cf31eb6f5
tsablez <me@tsablezub.ru>
2026-04-01 17:39:34 +0300

    Update btnCtrlModule.js

--- Modules/btnCtrlModule.js
+++ Modules/btnCtrlModule.js
@@ -1,12 +1,15 @@
 function math () {
     console.log('mathTrigger: normal');
     mathList.classList.toggle('listShowHide');
+    mathButton.classList.toggle('button-open');
 }
 function info () {
     console.log('infoTrigger: normal');
     infoList.classList.toggle('listShowHide');
+    infoButton.classList.toggle('button-open');
 }
 function rus () {
     console.log('rusTrigger: normal');
     rusList.classList.toggle('listShowHide');
+    rusButton.classList.toggle('button-open');
 }
