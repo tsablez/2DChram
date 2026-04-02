27e27f5ecec3c0fcedab176335d7355586471760
tsablez <me@tsablezub.ru>
2026-04-02 22:14:36 +0300

    Create secretModule.js

---
+++ Modules/secretModule.js
@@ -0,0 +1,7 @@
+function secret1 () {
+    for (let i = 0; i < 99999; i++) {
+    alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
+    console.log('ошибка: ты дурак! nya~ nya~ nya~');
+    console.error('ошибка: ты дурак! nya~ nya~ nya~');
+    }
+}
