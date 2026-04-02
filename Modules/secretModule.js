7f2cfd4d23de7885a4cd13e01976274e57cb4f60
tsablez <me@tsablezub.ru>
2026-04-02 22:21:14 +0300

    Update secretModule.js

--- Modules/secretModule.js
+++ Modules/secretModule.js
@@ -1,5 +1,19 @@
 function secret1 () {
-    for (let i = 0; i < 99999; i++) {
+    for (let i = 0; i < 9999999999; i++) {
+    alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
+    console.log('ошибка: ты дурак! nya~ nya~ nya~');
+    console.error('ошибка: ты дурак! nya~ nya~ nya~');
+    }
+}
+function secret11 () {
+    for (let i = 0; i < 9999999999; i++) {
+    alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
+    console.log('ошибка: ты дурак! nya~ nya~ nya~');
+    console.error('ошибка: ты дурак! nya~ nya~ nya~');
+    }
+}
+function secret111 () {
+    for (let i = 0; i < 9999999999; i++) {
     alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
     console.log('ошибка: ты дурак! nya~ nya~ nya~');
     console.error('ошибка: ты дурак! nya~ nya~ nya~');
