2bf51157a840717b2b06952a39518f106fb85208
tsablez <me@tsablezub.ru>
2026-04-04 15:12:42 +0300

    Add files via upload

---
+++ mainScript.js
@@ -0,0 +1,36 @@
+console.log('2D Храм by tsablez');
+
+function settings () { //JS для настроек
+    settingsList.classList.toggle('listHide');
+    settingsButton.classList.toggle('button-open');
+}
+function anime () {
+    animeButton.classList.toggle('button-open');
+}
+function manga () {
+    mangaButton.classList.toggle('button-open');
+}
+
+function changeTheme () { //JS для смены темы
+    theme.classList.toggle('changeTheme');
+}
+
+
+function secret1 () { //JS для пасхалочки
+    for (let i = 0; i < 9999999999999999999999999; i++) {
+        secret11();
+    }
+}
+function secret11 () {
+    alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
+    console.log('ошибка: ты дурак! nya~ nya~ nya~');
+    console.error('ошибка: ты дурак! nya~ nya~ nya~');
+    for (let i = 0; i < 9999999999999999999999999; i++) {
+        secret111();
+    }
+}
+function secret111() {
+    alert('Ошибка: ты дурачок, сказали же - не трогать)) nya~');
+    console.log('ошибка: ты дурак! nya~ nya~ nya~');
+    console.error('ошибка: ты дурак! nya~ nya~ nya~');
+}
